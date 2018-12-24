dd6172a5d167c0faaf7ceaf49f512e9b3b38ef2c
elishakay <kramer1346@gmail.com>
2018-12-24 14:39:06 +0200

    env config

--- 06_replier_bot/config-empty.js
+++ 06_replier_bot/config-empty.js
@@ -5,8 +5,8 @@
 // var config = require('./config.js');
 
 module.exports = {
-  consumer_key:         '', 
-  consumer_secret:      '',
-  access_token:         '',
-  access_token_secret:  ''
+  consumer_key: process.env.CONSUMER_KEY, 
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
 }
